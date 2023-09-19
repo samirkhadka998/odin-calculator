91ea759acbe0b87bb40e9a75139fab07ace2c56f
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-19 21:29:20 +0545

    Initial design of calculator , css changes , js to show clicked content on screen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('hello')
+
+const inputDiv = document.querySelector('.inputs');
+
+const screen = document.querySelector('.screen');
+
+let screenText = '';
+
+
+var operatordivs = document.querySelectorAll('.subinputs div');
+
+Array.from(operatordivs).forEach(d => {
+    d.addEventListener('click', logInfo)
+})
+
+function logInfo(e){
+    screenText += e.target.innerText + ' ';
+    screen.innerText = screenText;
+    console.log(e.target.innerText)
+
+}
